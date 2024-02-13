4c4fe08768a7802c22fd746e4903b36866110bbc
sunilcostacloud <sunil@costacloud.com>
2024-02-13 12:49:42 +0530

    update

--- home/src/components/Home.jsx
+++ home/src/components/Home.jsx
@@ -3,20 +3,49 @@ import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const navigate = useNavigate();
+
+  const handleAuthLoginNavigate = () => {
+    window.dispatchEvent(
+      new CustomEvent("[home] navigated", { detail: "/auth/login" })
+    );
+  };
+
+  const handleAuthLogoutNavigate = () => {
+    window.dispatchEvent(
+      new CustomEvent("[home] navigated", { detail: "/auth/logout" })
+    );
+  };
+
+  const handleProductListNavigate = () => {
+    window.dispatchEvent(
+      new CustomEvent("[home] navigated", { detail: "/products/products-list" })
+    );
+  };
+
+  const handleProductDetailsNavigate = () => {
+    window.dispatchEvent(
+      new CustomEvent("[home] navigated", {
+        detail: "/products/product/:123",
+      })
+    );
+  };
+
+  const handleUnauthorizedNavigate = () => {
+    window.dispatchEvent(
+      new CustomEvent("[home] navigated", { detail: "/unauthorized" })
+    );
+  };
+
   return (
     <div>
       <h1>This is Home Page</h1>
       <button onClick={() => navigate("/")}>Home</button>
       <button onClick={() => navigate("/pricing")}>Pricing</button>
-      <button onClick={() => navigate("/auth/login")}>Login</button>
-      <button onClick={() => navigate("/auth/logout")}>Logout</button>
-      <button onClick={() => navigate("/products/products-list")}>
-        Products
-      </button>
-      <button onClick={() => navigate("/products/product/:123")}>
-        product details
-      </button>
-      <button onClick={() => navigate("/unauthorized")}>Unauthorized</button>
+      <button onClick={handleAuthLoginNavigate}>Login</button>
+      <button onClick={handleAuthLogoutNavigate}>Logout</button>
+      <button onClick={handleProductListNavigate}>Products</button>
+      <button onClick={handleProductDetailsNavigate}>product details</button>
+      <button onClick={handleUnauthorizedNavigate}>Unauthorized</button>
       <button onClick={() => navigate("/sdf")}>Page not Found</button>
     </div>
   );
